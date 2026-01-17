3852e0e01311965aed74c9d5e5999042528bf0ba
tecnologisticaaduanal <sistemas2@tecnologisticaaduanal.com>
2026-01-17 11:14:27 -0600

    Se agrega la variable de entorno que retorna al entorno de desarrollo

--- view/Logout/logout.php
+++ view/Logout/logout.php
@@ -1,5 +1,12 @@
 <?php
     require_once("../../config/conexion.php");
+    require_once("../../vendor/autoload.php");
+    
+    use Dotenv\Dotenv;
+
+	$config = App\Config::getInstance();
+    $dotenv = Dotenv::createImmutable($config->getEnvPath(), '.env.' . $config->getEnvironment());
+    $dotenv->load();
     session_destroy();
     header("Location:"."http://localhost:80/TLA_Revision_Docs/"."index.php");  
 
