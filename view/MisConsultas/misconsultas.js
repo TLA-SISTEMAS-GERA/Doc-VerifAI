0929ff1b5cac4b665ac6a86c278074fac39dd850
GerardoCruz163 <gerarojo243@gmail.com>
2026-02-03 17:17:40 -0600

    correccion ruta btn consulta

--- view/MisConsultas/misconsultas.js
+++ view/MisConsultas/misconsultas.js
@@ -74,8 +74,8 @@ $(document).on("click",".btn-inline","url-inline",function(){
     //DATO TEMPORAL (MIENTRAS EL USUARIO ENTRE A LA CONSULTA, EL VALOR EXISTIRÁ)
     sessionStorage.setItem("id_real", realId);                              
     
-    window.open('http://localhost:80/Doc-VerifAI/view/DetalleConsulta/?ID='+ciphertext+'');
-    //window.open('http://doc-verifai.tecnologisticaaduanal.com/view/DetalleConsulta/?ID='+ciphertext+'');
+    //window.open('http://localhost:80/Doc-VerifAI/view/DetalleConsulta/?ID='+ciphertext+'');
+    window.open('http://doc-verifai.tecnologisticaaduanal.com/view/DetalleConsulta/?ID='+ciphertext+'');
     
 });
 
