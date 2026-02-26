db3195999b9aadad8861cb819e62e0470a6fcb15
GerardoCruz163 <gerarojo243@gmail.com>
2026-02-26 10:48:30 -0600

    Correccion logout.php a dominio de server

--- view/Logout/logout.php
+++ view/Logout/logout.php
@@ -1,6 +1,6 @@
 <?php
     require_once("../../config/conexion.php");
     session_destroy();
-    header("Location:"."http://localhost:80/Doc-VerifAI/"."index.php");  
-   //header("Location:"."http://doc-verifai.tecnologisticaaduanal.com/"."index.php");  
+   // header("Location:"."http://localhost:80/Doc-VerifAI/"."index.php");  
+   header("Location:"."http://doc-verifai.tecnologisticaaduanal.com/"."index.php");  
 ?>
